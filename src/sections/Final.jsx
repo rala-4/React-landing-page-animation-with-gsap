b31f2aa21a6832dfea3b6953082dd9d44fb90b1a
Rala Kous <rolenokous@gmail.com>
2025-11-14 15:31:48 +0300

    Edit Final timeline

--- src/sections/Final.jsx
+++ src/sections/Final.jsx
@@ -8,15 +8,15 @@ function Final() {
   useGSAP(() => {
     gsap.set(".final-content", { opacity: 0 });
 
-    // gsap.timeline({
-    //   scrollTrigger: {
-    //     trigger: ".final",
-    //     start: "top top",
-    //     end: "90% top",
-    //     scrub: true,
-    //     pin: true,
-    //   },
-    // });
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: ".final",
+        start: "top top",
+        end: "90% top",
+        scrub: true,
+        pin: true,
+      },
+    });
 
     const tl = gsap.timeline({
       scrollTrigger: {
